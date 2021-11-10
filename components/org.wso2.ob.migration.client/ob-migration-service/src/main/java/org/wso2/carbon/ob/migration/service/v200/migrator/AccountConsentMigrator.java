cf592c0382f5d5616aad67708d186b89c48e580e
DivyaPremanantha <premananthadivya@gmail.com>
2021-11-10 12:52:51 +0530

    Add migrator sample query

--- components/org.wso2.ob.migration.client/ob-migration-service/src/main/java/org/wso2/carbon/ob/migration/service/v200/migrator/AccountConsentMigrator.java
+++ components/org.wso2.ob.migration.client/ob-migration-service/src/main/java/org/wso2/carbon/ob/migration/service/v200/migrator/AccountConsentMigrator.java
@@ -5,8 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.wso2.carbon.identity.core.migrate.MigrationClientException;
 import org.wso2.carbon.ob.migration.service.Migrator;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AccountConsentMigrator extends Migrator {
 
+    private Connection conn = null;
     private static final Logger log = LoggerFactory.getLogger(AccountConsentMigrator.class);
 
     @Override
@@ -15,6 +21,26 @@ public class AccountConsentMigrator extends Migrator {
     }
 
     public void migrate() throws MigrationClientException {
-        log.info("Migrate");
+        try (Connection connection = getDataSource().getConnection()) {
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from OB_CONSENT");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            String t = "";
+            while (resultSet.next()) {
+                t = resultSet.getString("CONSENT_ID");
+            }
+            log.error("CONSENT_ID: " + t);
+
+            PreparedStatement preparedStatement3 = connection.prepareStatement("select * from UK_ACCOUNT_INITIATION");
+            ResultSet resultSet3 = preparedStatement3.executeQuery();
+            String t3 = "";
+            while (resultSet3.next()) {
+                t3 = resultSet3.getString("REQUEST");
+            }
+            log.info("REQUEST: " + t3);
+        } catch (
+                SQLException e) {
+            throw new MigrationClientException("Failed to migrate permissions.", e);
+        }
     }
 }
