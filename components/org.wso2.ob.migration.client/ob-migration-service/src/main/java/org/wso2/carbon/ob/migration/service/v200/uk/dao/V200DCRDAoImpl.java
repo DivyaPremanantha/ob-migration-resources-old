5c9e3b5f94db887eb320370d234323cb698ce36d
DivyaPremanantha <premananthadivya@gmail.com>
2021-11-26 14:37:41 +0530

    Add licence header

--- components/org.wso2.ob.migration.client/ob-migration-service/src/main/java/org/wso2/carbon/ob/migration/service/v200/uk/dao/V200DCRDAoImpl.java
+++ components/org.wso2.ob.migration.client/ob-migration-service/src/main/java/org/wso2/carbon/ob/migration/service/v200/uk/dao/V200DCRDAoImpl.java
@@ -1,10 +1,20 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses. For specific
+ * language governing the permissions and limitations under this license,
+ * please see the license as well as any agreement you’ve entered into with
+ * WSO2 governing the purchase of this software and any associated services.
+ */
 package org.wso2.carbon.ob.migration.service.v200.uk.dao;
 
 import com.wso2.openbanking.accelerator.common.exception.OpenBankingException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.ob.migration.service.v200.uk.model.DCRModel;
-import org.wso2.carbon.ob.migration.service.v200.uk.model.UKConsentInitiationModel;
 import org.wso2.carbon.ob.migration.service.v200.uk.queries.DCRSQLStatements;
 import org.wso2.carbon.ob.migration.service.v200.uk.util.ConsentDaoUtil;
 
@@ -43,10 +53,5 @@ public class V200DCRDAoImpl implements V200DCRDao {
         }
 
         return dcrModels;
-    }
-
-    @Override
-    public List<DCRModel> updateSPMetadata(Connection connection) throws OpenBankingException {
-        return null;
     }
 }
